436d9ff27ddabf9f4c5348035ee3ce83e84b5a68
Therese Lybo <therese.lybo@gmail.com>
2020-08-31 14:42:12 +0200

    Added all files

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      borderRadius: {
+        'lg': '2rem'
+      },
+      height: {
+        "450px": "450px",
+        "30px": "30px",
+        "5px": "5px",
+        "144px": "144px"
+      },
+      width: {
+        "300px": "300px",
+        "30px": "30px",
+        "5px": "5px",
+        "144px": "144px"
+      },
+      margin: {
+        "24": "7rem",
+        "-16": "-0.16rem"
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+}
